347f3bfd90d548c1ccf662c240cb4654afaab489
banzmahoree <tanawatnayada@hotmail.com>
2019-04-24 10:17:16 +0700

    commit

--- app.js
+++ app.js
@@ -118,5 +118,3 @@ var server = app.listen(process.env.PORT || 5000, function () {
   var port = server.address().port;
   console.log("Express is working on port " + port);
 });
-
-module.exports = app;
